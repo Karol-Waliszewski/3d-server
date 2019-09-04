a2fcd11cded455fb80bca29cef19bc060ae2fc5d
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-09-04 10:36:14 +0200

    t

--- placeholders.js
+++ placeholders.js
@@ -1,216 +1,213 @@
-const FLATS = {
-  flats: [
-    {
-      lokal: "35",
-      metraz: "29.42",
-      status: "Dostepne",
-      cena: "127357.8",
-      pokoje: "5",
-      pietro: "0",
-      Budynek: "A",
-      Lokal_str: "A35"
-    },
-    {
-      lokal: "53",
-      metraz: "29.42",
-      status: "Sprzedane",
-      cena: "127357.8",
-      pokoje: "4",
-      pietro: "0",
-      Budynek: "A",
-      Lokal_str: "A53"
-    },
-    {
-      lokal: "3",
-      metraz: "21.42",
-      status: "Dostepne",
-      cena: "127357.8",
-      pokoje: "2",
-      pietro: "0",
-      Budynek: "A",
-      Lokal_str: "A3"
-    },
-    {
-      lokal: "34",
-      metraz: "28.42",
-      status: "Zarezerwowane",
-      cena: "127357.8",
-      pokoje: "1",
-      pietro: "0",
-      Budynek: "A",
-      Lokal_str: "A34"
-    },
-    {
-      lokal: "52",
-      metraz: "28.42",
-      status: "Dostepne",
-      cena: "127357.8",
-      pokoje: "1",
-      pietro: "0",
-      Budynek: "A",
-      Lokal_str: "A52"
-    },
-    {
-      lokal: "6",
-      metraz: "129.42",
-      status: "Dostepne",
-      cena: "107357.8",
-      pokoje: "3",
-      pietro: "1",
-      Budynek: "A",
-      Lokal_str: "A6"
-    },
-    {
-      lokal: "56",
-      metraz: "89.42",
-      status: "Sprzedane",
-      cena: "9000.8",
-      pokoje: "2",
-      pietro: "1",
-      Budynek: "A",
-      Lokal_str: "A56"
-    },
-    {
-      lokal: "76",
-      metraz: "89.42",
-      status: "Zarezerwowane",
-      cena: "90000.8",
-      pokoje: "4",
-      pietro: "1",
-      Budynek: "A",
-      Lokal_str: "A76"
-    },
-    {
-      lokal: "39",
-      metraz: "177.42",
-      status: "Dostepne",
-      cena: "107357.8",
-      pokoje: "3",
-      pietro: "2",
-      Budynek: "A",
-      Lokal_str: "A39"
-    },
-    {
-      lokal: "57",
-      metraz: "42.42",
-      status: "Sprzedane",
-      cena: "9000.8",
-      pokoje: "2",
-      pietro: "2",
-      Budynek: "A",
-      Lokal_str: "A57"
-    },
-    {
-      lokal: "38",
-      metraz: "45.42",
-      status: "Zarezerwowane",
-      cena: "90000.8",
-      pokoje: "4",
-      pietro: "2",
-      Budynek: "A",
-      Lokal_str: "A38"
-    },
-    {
-      lokal: "23",
-      metraz: "177.42",
-      status: "Dostepne",
-      cena: "107357.8",
-      pokoje: "3",
-      pietro: "3",
-      Budynek: "A",
-      Lokal_str: "A23"
-    },
-    {
-      lokal: "20",
-      metraz: "42.42",
-      status: "Sprzedane",
-      cena: "9000.8",
-      pokoje: "2",
-      pietro: "3",
-      Budynek: "A",
-      Lokal_str: "A20"
-    },
-    {
-      lokal: "21",
-      metraz: "45.42",
-      status: "Zarezerwowane",
-      cena: "90000.8",
-      pokoje: "4",
-      pietro: "3",
-      Budynek: "A",
-      Lokal_str: "A21"
-    },
-    {
-      lokal: "27",
-      metraz: "177.42",
-      status: "Dostepne",
-      cena: "107357.8",
-      pokoje: "3",
-      pietro: "4",
-      Budynek: "A",
-      Lokal_str: "A27"
-    },
-    {
-      lokal: "29",
-      metraz: "42.42",
-      status: "Sprzedane",
-      cena: "9000.8",
-      pokoje: "2",
-      pietro: "4",
-      Budynek: "A",
-      Lokal_str: "A29"
-    },
-    {
-      lokal: "28",
-      metraz: "45.42",
-      status: "Zarezerwowane",
-      cena: "90000.8",
-      pokoje: "4",
-      pietro: "4",
-      Budynek: "A",
-      Lokal_str: "A28"
-    },
-    {
-      lokal: "26",
-      metraz: "177.42",
-      status: "Dostepne",
-      cena: "1357.8",
-      pokoje: "3",
-      pietro: "2",
-      Budynek: "A",
-      Lokal_str: "A26"
-    },
-    {
-      lokal: "25",
-      metraz: "42.42",
-      status: "Sprzedane",
-      cena: "1200.8",
-      pokoje: "2",
-      pietro: "4",
-      Budynek: "A",
-      Lokal_str: "A25"
-    },
-    {
-      lokal: "101",
-      metraz: "45.42",
-      status: "Zarezerwowane",
-      cena: "977000.8",
-      pokoje: "2",
-      pietro: "1",
-      Budynek: "A",
-      Lokal_str: "A101"
-    },
-    {
-      lokal: "101",
-      metraz: "45.42",
-      status: "Zarezerwowane",
-      cena: "977000.8",
-      pokoje: "2",
-      pietro: "1",
-      Budynek: "B",
-      Lokal_str: "B101"
-    }
-  ]
-};
+const FLATS = [
+  {
+    lokal: "35",
+    metraz: "29.42",
+    status: "Dostepne",
+    cena: "127357.8",
+    pokoje: "5",
+    pietro: "0",
+    Budynek: "A",
+    Lokal_str: "A35"
+  },
+  {
+    lokal: "53",
+    metraz: "29.42",
+    status: "Sprzedane",
+    cena: "127357.8",
+    pokoje: "4",
+    pietro: "0",
+    Budynek: "A",
+    Lokal_str: "A53"
+  },
+  {
+    lokal: "3",
+    metraz: "21.42",
+    status: "Dostepne",
+    cena: "127357.8",
+    pokoje: "2",
+    pietro: "0",
+    Budynek: "A",
+    Lokal_str: "A3"
+  },
+  {
+    lokal: "34",
+    metraz: "28.42",
+    status: "Zarezerwowane",
+    cena: "127357.8",
+    pokoje: "1",
+    pietro: "0",
+    Budynek: "A",
+    Lokal_str: "A34"
+  },
+  {
+    lokal: "52",
+    metraz: "28.42",
+    status: "Dostepne",
+    cena: "127357.8",
+    pokoje: "1",
+    pietro: "0",
+    Budynek: "A",
+    Lokal_str: "A52"
+  },
+  {
+    lokal: "6",
+    metraz: "129.42",
+    status: "Dostepne",
+    cena: "107357.8",
+    pokoje: "3",
+    pietro: "1",
+    Budynek: "A",
+    Lokal_str: "A6"
+  },
+  {
+    lokal: "56",
+    metraz: "89.42",
+    status: "Sprzedane",
+    cena: "9000.8",
+    pokoje: "2",
+    pietro: "1",
+    Budynek: "A",
+    Lokal_str: "A56"
+  },
+  {
+    lokal: "76",
+    metraz: "89.42",
+    status: "Zarezerwowane",
+    cena: "90000.8",
+    pokoje: "4",
+    pietro: "1",
+    Budynek: "A",
+    Lokal_str: "A76"
+  },
+  {
+    lokal: "39",
+    metraz: "177.42",
+    status: "Dostepne",
+    cena: "107357.8",
+    pokoje: "3",
+    pietro: "2",
+    Budynek: "A",
+    Lokal_str: "A39"
+  },
+  {
+    lokal: "57",
+    metraz: "42.42",
+    status: "Sprzedane",
+    cena: "9000.8",
+    pokoje: "2",
+    pietro: "2",
+    Budynek: "A",
+    Lokal_str: "A57"
+  },
+  {
+    lokal: "38",
+    metraz: "45.42",
+    status: "Zarezerwowane",
+    cena: "90000.8",
+    pokoje: "4",
+    pietro: "2",
+    Budynek: "A",
+    Lokal_str: "A38"
+  },
+  {
+    lokal: "23",
+    metraz: "177.42",
+    status: "Dostepne",
+    cena: "107357.8",
+    pokoje: "3",
+    pietro: "3",
+    Budynek: "A",
+    Lokal_str: "A23"
+  },
+  {
+    lokal: "20",
+    metraz: "42.42",
+    status: "Sprzedane",
+    cena: "9000.8",
+    pokoje: "2",
+    pietro: "3",
+    Budynek: "A",
+    Lokal_str: "A20"
+  },
+  {
+    lokal: "21",
+    metraz: "45.42",
+    status: "Zarezerwowane",
+    cena: "90000.8",
+    pokoje: "4",
+    pietro: "3",
+    Budynek: "A",
+    Lokal_str: "A21"
+  },
+  {
+    lokal: "27",
+    metraz: "177.42",
+    status: "Dostepne",
+    cena: "107357.8",
+    pokoje: "3",
+    pietro: "4",
+    Budynek: "A",
+    Lokal_str: "A27"
+  },
+  {
+    lokal: "29",
+    metraz: "42.42",
+    status: "Sprzedane",
+    cena: "9000.8",
+    pokoje: "2",
+    pietro: "4",
+    Budynek: "A",
+    Lokal_str: "A29"
+  },
+  {
+    lokal: "28",
+    metraz: "45.42",
+    status: "Zarezerwowane",
+    cena: "90000.8",
+    pokoje: "4",
+    pietro: "4",
+    Budynek: "A",
+    Lokal_str: "A28"
+  },{
+    lokal: "26",
+    metraz: "177.42",
+    status: "Dostepne",
+    cena: "1357.8",
+    pokoje: "3",
+    pietro: "2",
+    Budynek: "A",
+    Lokal_str: "A26"
+  },
+  {
+    lokal: "25",
+    metraz: "42.42",
+    status: "Sprzedane",
+    cena: "1200.8",
+    pokoje: "2",
+    pietro: "4",
+    Budynek: "A",
+    Lokal_str: "A25"
+  },
+  {
+    lokal: "101",
+    metraz: "45.42",
+    status: "Zarezerwowane",
+    cena: "977000.8",
+    pokoje: "2",
+    pietro: "1",
+    Budynek: "A",
+    Lokal_str: "A101"
+  },
+  {
+    lokal: "101",
+    metraz: "45.42",
+    status: "Zarezerwowane",
+    cena: "977000.8",
+    pokoje: "2",
+    pietro: "1",
+    Budynek: "B",
+    Lokal_str: "B101"
+  },
+];
 
 module.exports = FLATS;
